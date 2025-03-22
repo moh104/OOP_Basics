8038e1c149f3bbb0f8939507f39f28c430881d81
moh104 <mg1game1m@gmail.com>
2025-03-22 21:31:30 +0330

    Add constructor & destructor for Event

---
+++ ClendarEvent/event.cpp
@@ -0,0 +1,15 @@
+#include <ctime>
+#include "event.hpp"
+
+using namespace std;
+
+Event::Event(const string& i_name , time_t i_start , time_t i_end) : start{i_start} , end{i_end}
+{
+    set_name(i_name);
+    validationTimes(i_start , i_end);
+}
+
+Event::~Event() noexcept
+{
+    cout << "Event '" << name << "' has been deleted.\n";
+}
