7b9463613d5e92b7f66d2e0eab615db000737f1b
moh104 <mg1game1m@gmail.com>
2025-03-25 17:11:37 +0330

    add other functions for Character

--- GameCharacter/character.cpp
+++ GameCharacter/character.cpp
@@ -57,6 +57,131 @@ Character::Character(const string& i_name , int i_health , int i_power)
     initialSkills();
 }
 
+void Character::set_name(const string& i_name)
+{
+    if (i_name.empty())
+    {
+        throw invalid_argument("Character name cannot be empty.");
+    }
+    name = i_name;
+}
+
+void Character::set_health(int i_health)
+{
+    if (i_health < MIN_HEALTH || i_health > MAX_HEALTH)
+    {
+        throw invalid_argument("The level of health must be between 0 and 100.");
+    }
+    health = i_health;
+}
+
+void Character::set_power(int i_power)
+{
+    if (i_power < MIN_POWER || i_power > MAX_POWER)
+    {
+        throw invalid_argument("The level of power must be between 0 and 100.");
+    }
+    power = i_power;
+}
+
+string Character::get_name() const noexcept
+{
+    return name;
+}
+
+void Character::add_skill(const string& skillName , int level)
+{
+    int index{find_index(skillName)};
+    if (index != -1)
+    {
+        skills[index].set_level(level);
+        cout << "Skill level '" << skillName << "' updated.\n";
+    }
+    else
+    {
+        if (skills.size() == skills.capacity()) [[unlikely]]
+        {
+            cout << "The skills capacity for character is full. So to create new skills capacity, a new memory with more capacity is created and the previous skills are copied to the new memory and then deleted.\n";
+        }
+        skills.emplace_back(skillName , level);
+        cout << "Skill added successfully.\n";
+    }
+}
+
+void Character::remove_skill(const std::string& skillName)
+{
+    int index{find_index(skillName)};
+    if (index == -1)
+    {
+        throw std::invalid_argument("The desired skill was not found.");
+    }
+    skills.erase(skills.begin() + index);
+}
+
+void Character::print() const noexcept
+{
+    cout << "Name: " << name << "\n";
+    cout << "Health: " << health << "\n";
+    cout << "Power: " << power << "\n";
+    cout << "----------\nSkills:\n";
+    if (skills.empty())
+    {
+        std::cout << "There is no skill.\n";
+    }
+    else
+    {
+        for (const auto& skill : skills)
+        {
+            std::cout << "  " << skill.get_name() << " : lvl " << skill.get_level() << "\n";
+        }
+    }
+    cout << "----------\n";
+}
+
+void Character::initialSkills()
+{
+    int numSkills;
+    while (true)
+    {
+        numSkills = get_intNumber("Specify the number of skills to initialize: ");
+        if (numSkills >= 0)
+            break;
+
+        cerr << "The number of skills cannot be negative. Please try again.\n";
+    }
+
+    for (int i = 0 ; i < numSkills ;)
+    {
+        cout << "Enter Skill name: ";
+        string skillName;
+        getline(cin , skillName);
+        int level{get_intNumber("Enter Skill level (0 to 50): ")};
+        try
+        {
+            add_skill(skillName , level);
+            i++;
+        }
+        catch(const invalid_argument& e)
+        {
+            cout << "(ERROR) " << e.what() << "Fix this skill again.\n";
+        }
+    }
+}
+
+int Character::find_index(const string& skillName) const noexcept
+{
+    int index{0};
+    for (const auto& skill : skills)
+    {
+        if (skill.get_name() == skillName) [[unlikely]]
+            return index;
+        
+        index++;
+    }
+
+    return -1;
+}
+
 Character::~Character() noexcept
 {
     cout << "Character '" << name << "' deleted.\n";
