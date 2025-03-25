a1fc3fdd3b20d6119b9b5c411b171a6c2a34915b
moh104 <mg1game1m@gmail.com>
2025-03-25 22:19:52 +0330

    add main

--- NightSky/main.cpp
+++ NightSky/main.cpp
@@ -72,3 +72,95 @@ float get_floatNumber(const string& output)
         }
     }
 }
+
+int main()
+{
+    while(true)
+    {
+        int posX, posY, Vx, Vy;
+        cout << "Enter initial UFO position (x,y)\n";
+        posX = get_intNumber("X: ");
+        posY = get_intNumber("Y: ");
+        cout << "Enter UFO speed (Vx,Vy)\n";
+        Vx = get_intNumber("Vx: ");
+        Vy = get_intNumber("Vy: ");
+        try
+        {
+            Sky sky(posX, posY, Vx, Vy);
+            while (true)
+            {
+                int choice { get_intNumber("Enter command number(enter 4 to see the command menu): ") };
+                try
+                {
+                    if (choice == 1)
+                    {
+                        cout << "Enter star position (x,y)\n";
+                        int x { get_intNumber("X (0 to 800): ") };
+                        int y { get_intNumber("Y (0 to 600): ") };
+                        float brightness { get_floatNumber("Enter brightness (1.0 to 5.0): ") };
+                        int color { get_intNumber("1)White\n2)Yellow\n3)Gold\n4)pink\nEnter color number: ") };
+                        
+                        if (color == 1)
+                        {
+                            sky.add_star(x , y , brightness , WHITE);
+                            cout << "Star added successfully.\n";
+                        }
+                        else if (color == 2)
+                        {
+                            sky.add_star(x , y , brightness , YELLOW);
+                            cout << "Star added successfully.\n";
+                        }
+                        else if (color == 3)
+                        {
+                            sky.add_star(x , y , brightness , GOLD);
+                            cout << "Star added successfully.\n";
+                        }
+                        else if (color == 4)
+                        {
+                            sky.add_star(x , y , brightness , PINK);
+                            cout << "Star added successfully.\n";
+                        }
+                        else
+                            cout << "Invalid color number entered.\n";
+                    }
+                    else if (choice == 2)
+                    {
+                        cout << "Enter star position to remove (x,y)\n";
+                        int x { get_intNumber("X (0 to 600): ") };
+                        int y { get_intNumber("Y (0 to 800): ") };
+                        sky.remove_star(x , y);
+                        cout << "The star was successfully destroyed.\n";
+                    }
+                    else if (choice == 3)
+                    {
+                        sky.render();
+                    }
+                    else if (choice == 4)
+                    {
+                        menu();
+                    }
+                    else if (choice == 5)
+                    {
+                        cout << "Exiting program\n";
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Invalid choice! Please try again.\n";
+                    }
+                }
+                catch (const invalid_argument& e)
+                {
+                    cout << "(ERROR)" << e.what() << "\n";
+                }
+            }
+            break;
+        }
+        catch (const invalid_argument& e)
+        {
+            cout << "(ERROR)" << e.what() << "\nPlease try again.\n";
+        }
+    }
+
+    return 0;
+}
