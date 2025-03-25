671e94b615e3e6036df16ff5405b3383b7946f98
moh104 <mg1game1m@gmail.com>
2025-03-25 16:33:15 +0330

    add other functions for Skill

--- GameCharacter/character.cpp
+++ GameCharacter/character.cpp
@@ -12,6 +12,35 @@ Skill::Skill(const string& i_name , int i_level)
     set_level(i_level);
 }
 
+const string& Skill::get_name() const noexcept
+{
+    return name;
+}
+
+int Skill::get_level() const noexcept
+{
+    return level;
+}
+
+void Skill::set_name(const string& i_name)
+{
+    if (i_name.empty())
+    {
+        throw invalid_argument("Skill name cannot be empty.");
+    }
+    name = i_name;
+}
+
+void Skill::set_level(int i_level)
+{
+    if (i_level < MIN_LEVEL || i_level > MAX_LEVEL)
+    {
+        throw invalid_argument("The skill level must be between 0 and 50.");
+    }
+    level = i_level;
+}
+
+
 Skill::~Skill() noexcept
 {
     cout << "Skill '" << name << "' was removed.\n";
